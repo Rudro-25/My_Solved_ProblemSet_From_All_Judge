6825ab3fdce948d2feb36442b23febae516c0eb0
Rudro Debnath <56835340+Rudro-25@users.noreply.github.com>
2022-08-15 14:52:39 +0600

    Create Problem Makes Problem.cpp

---
+++ LightOj/Math/Problem Makes Problem.cpp
@@ -0,0 +1,62 @@
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable" 
+#pragma GCC diagnostic ignored "-Wformat"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define dhoom3   ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*ordered_set<ll> s;
+*s.find_by_order(x) //ans at x index in sorted order.
+s.order_of_key(x); //index of x or if not find x , it show index no in sort array.*/
+
+const int N= 2e5+5;
+const int NC=2000005;
+constexpr ll MOD = 1e9  + 7;
+ll inv[NC],fac_inv[NC],fac[NC];
+void initialize()
+{
+    ll i;
+    inv[1]=1;
+    for(i=2;i<=NC-2;i++)
+        inv[i]=(MOD-MOD/i)*inv[MOD%i]%MOD;
+    fac[0]=fac[1]=1;
+    for(i=2;i<=NC-2;i++)
+        fac[i]=i*fac[i-1]%MOD;
+    fac_inv[0]=fac_inv[1]=1;
+    for(i=2;i<=NC-2;i++)
+        fac_inv[i]=inv[i]*fac_inv[i-1]%MOD;
+}
+ll ncr(ll n,ll r)
+{
+    if(n<r) return 0;
+    return (fac[n]*fac_inv[r]%MOD)*fac_inv[n-r]%MOD;
+}
+
+int main(){
+   dhoom3; 
+   int Test=1;
+   initialize();
+   cin>>Test;
+   for(ll tt=1;tt<=Test;tt++){
+       ll n,k; cin>>n>>k;
+       cout<<"Case "<<tt<<": "<<ncr(n+k-1,k-1)<<endl;
+       
+       
+    
+   }
+   //cout << (ans ? "YES\n" : "No\n") ;
+   return 0;
+}
+/*
+
+*/
